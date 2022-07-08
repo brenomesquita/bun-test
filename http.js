d3fcbe3072c85ef2c4b03b06da0e008c6303bb1b
breno <brenomesquita>
2022-07-07 22:59:07 -0300

    docker

--- http.js
+++ http.js
@@ -1,6 +1,12 @@
 export default {
   port: 3000,
   fetch(request) {
-    return new Response("Welcome to Bun!");
+    return new Response("Welcome to Bun!",
+      {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      }
+    );
   },
 };
