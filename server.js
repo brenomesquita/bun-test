c572347c04aaad52498e2a745084298e9bee5a1a
breno <brenomesquita>
2022-07-07 23:22:50 -0300

    docker

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+let req = 0
 export default {
   port: Number(process.env.PORT ?? 3000),
   fetch(request) {
-    return new Response("Welcome to Bun!",
+    return new Response(`Welcome to Bun!${i=i+1}`,
       {
         headers: {
           "Content-Type": "text/html",
