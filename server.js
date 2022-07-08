185a6081fe68a7ff18f092c522276bbf8f42ac5d
breno <brenomesquita>
2022-07-07 23:05:42 -0300

    docker

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
 export default {
   port: Number(process.env.PORT ?? 3000),
   fetch(request) {
-    return new Response(
-      `
-      <html>
-        <main>
-          <img src="https://bun.sh/logo@2x.png" style="height: 48px;" alt="bun logo" />
-          <h1>Theo's Crappy Benchmark (Bun On Railway)</h1>
-          <h4>Rendered at: ${new Date().toISOString()}</h4>
-          <h2><span>Full request to render time (according to Theo): <span id="overrideme" />ms</span></h2>
-          <script>
-            const currentTime = new Date();
-            // round trip time
-            const fullTime = currentTime - window.performance.timing.requestStart;
-            document.getElementById('overrideme').innerHTML = fullTime.toString();
-            console.log('THEO REPORTS', fullTime);
-    
-            const times = JSON.parse(localStorage.getItem('astro-edge-times-store')) ?? [];
-            times.push(fullTime);
-            localStorage.setItem('astro-edge-times-store', JSON.stringify(times));
-            console.table(times);
-          </script>
-        </main>
-      </html>`,
+    return new Response("Welcome to Bun!",
       {
         headers: {
           "Content-Type": "text/html",
